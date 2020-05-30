ff3b756a045e20dc5e84ff1715dcb82bde9bd735
DimaOrekhov <d.i.orekhov@gmail.com>
2020-05-30 18:13:18 +0300

    movable and cameramover facets

---
+++ src/main/kotlin/ru/spb/mit/roboroguelike/systems/CameraMover.kt
@@ -0,0 +1,13 @@
+package ru.spb.mit.roboroguelike.systems
+
+import org.hexworks.amethyst.api.Command
+import org.hexworks.amethyst.api.Response
+import org.hexworks.amethyst.api.base.BaseFacet
+import org.hexworks.amethyst.api.entity.EntityType
+import ru.spb.mit.roboroguelike.GameContext
+
+class CameraMover: BaseFacet<GameContext>() {
+    override fun executeCommand(command: Command<out EntityType, GameContext>): Response {
+        TODO("Not yet implemented")
+    }
+}
